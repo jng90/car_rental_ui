b23b30c2d54761d5638563c6721c1a7397868f98
jng90 <jng@o2.pl>
2021-10-30 11:17:12 +0200

    Beggining

---
+++ src/components/AppHeader.js
@@ -0,0 +1,53 @@
+import classes from "./AppHeader.module.css";
+import {Link} from 'react-router-dom'
+import logo from "../logo.svg";
+import Storage from "@material-ui/icons/Storage";
+import AddComment from "@material-ui/icons/AddComment";
+
+const HEADER_BUTTONS = [
+    {
+        name: 'Home',
+        href: '/',
+        icon: (<></>),  /*Braj Ikony*/
+    },
+    {
+        name: 'Database', /* link do tablicy z listą rekordów/danych */
+        href: '/database',
+        icon: (<Storage/>),  /*Braj Ikony*/
+    },
+    {
+        name: 'Form',
+        href: '/form',
+        icon: (<AddComment/>),
+    },
+]
+
+
+const AppHeader = () => {
+
+    const mapToHeaderButton = (buttonInfo) => {
+        return (
+            <Link to={buttonInfo.href}>
+            <div className={classes.HeaderMenuButton}>
+                {buttonInfo.icon}
+                <div>{buttonInfo.name}</div>
+            </div>
+            </Link>
+        )
+    }
+
+    return (
+        <header className={classes.AppHeader}>
+            <div className={classes.HeaderLeft}>
+                <img src={logo} className={classes.AppLogo} alt="logo"/>
+            </div>
+            <div className={classes.HeaderRight}>
+                {
+                    HEADER_BUTTONS.map(mapToHeaderButton)
+                }
+            </div>
+        </header>
+    );
+
+}
+export default AppHeader;
